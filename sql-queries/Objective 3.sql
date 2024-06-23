bfb052a578eacb63c5e5b070c64fc845841c9497
Nukendra Pradeep Raju Thota <148835589+nukendrathota@users.noreply.github.com>
2024-06-23 13:45:57 -0400

    Add files via upload

--- sql-queries/Objective 3.sql
+++ sql-queries/Objective 3.sql
@@ -8,6 +8,7 @@ FROM order_details O
 LEFT JOIN menu_items M ON O.item_id = M.menu_item_id
 ;
 
+#2. What were the number of orders for each category?
 SELECT category, COUNT(*) AS no_of_orders
 FROM order_details O
 LEFT JOIN menu_items M ON O.item_id = M.menu_item_id
@@ -15,7 +16,7 @@ GROUP BY 1
 ORDER BY 2 DESC
 ;
 
-#2. What were the least and most ordered items? What categories were they in?
+#3. What were the least and most ordered items? What categories were they in?
 SELECT category, item_name, COUNT(*) AS no_of_orders
 FROM order_details O
 LEFT JOIN menu_items M ON O.item_id = M.menu_item_id
@@ -23,7 +24,7 @@ GROUP BY 2, 1
 ORDER BY 3 DESC
 ;  
 
-#3. What were the top 5 orders that spent the most money?
+#4. What were the top 5 orders that spent the most money?
 SELECT order_id, SUM(price) AS order_total
 FROM order_details O
 LEFT JOIN menu_items M ON O.item_id = M.menu_item_id
@@ -31,7 +32,7 @@ GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 5;
 
-#4. View the details of the highest spend order. Which specific items were purchased?
+#5. View the details of the highest spend order. Which specific items were purchased?
 SELECT category, 
 	   item_name, 
        COUNT(item_id) OVER (partition by category) as num_items_per_category, 
@@ -42,7 +43,7 @@ WHERE order_id = 440
 ORDER BY 4 DESC
 ;
 
-#5. View the details of the top 5 highest spend orders.
+#6. View the details of the top 5 highest spend orders.
 SELECT distinct category,   
        COUNT(item_id) OVER (partition by category) as num_items_per_category, 
        SUM(price) OVER (partition by category) AS total_spent_per_category
